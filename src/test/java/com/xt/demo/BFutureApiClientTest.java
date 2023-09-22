6ab046d8fd52659d2d3c834c4f9ac1f2badaa969
zhuang.zhou <1015525379@qq.com>
2023-09-22 14:49:44 +0800

    future api and spot api

---
+++ src/test/java/com/xt/demo/BFutureApiClientTest.java
@@ -0,0 +1,98 @@
+package com.xt.demo;
+
+import com.google.gson.Gson;
+import com.xt.api.client.future.XtFutureApiClient;
+import com.xt.api.client.future.XtUFutureApiClientImpl;
+import com.xt.api.dto.FutureCommonResponse;
+import com.xt.api.dto.future.FuturePostOrderRequest;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @author zhouzhuang
+ * @create 2023/9/20 16:15
+ */
+public class UFutureApiClientTest {
+    Gson gson = new Gson();
+    XtFutureApiClient client = new XtUFutureApiClientImpl(null);
+
+    @Test
+    public void postOrder(){
+        FuturePostOrderRequest request = FuturePostOrderRequest.builder().symbol("btc_usdt")
+                .origQty("4")
+                .orderType("LIMIT")
+                .timeInForce("GTC")
+                .price("26972.9")
+                .orderSide("BUY")
+                .positionSide("LONG")
+                .build();
+        FutureCommonResponse commonResponse = client.postOrder(request);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void batchOrder(){
+        FuturePostOrderRequest request = FuturePostOrderRequest.builder().symbol("btc_usdt")
+                .origQty("4")
+                .orderType("LIMIT")
+                .timeInForce("GTC")
+                .price("26972.9")
+                .orderSide("BUY")
+                .positionSide("LONG")
+                .build();
+        FuturePostOrderRequest request2 = FuturePostOrderRequest.builder().symbol("btc_usdt")
+                .origQty("4")
+                .orderType("LIMIT")
+                .timeInForce("GTC")
+                .price("26972.9")
+                .orderSide("BUY")
+                .positionSide("LONG")
+                .build();
+        List<FuturePostOrderRequest> requestList = new ArrayList<>();
+        requestList.add(request);
+        requestList.add(request2);
+        FutureCommonResponse commonResponse = client.batchOrder(requestList);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void orderListHistory() {
+        Map<String, String> params = new HashMap<>();
+        FutureCommonResponse commonResponse = client.orderListHistory(params);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void orderTradeList() {
+        Map<String, String> params = new HashMap<>();
+        FutureCommonResponse commonResponse = client.orderTradeList(params);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void orderDetail() {
+        FutureCommonResponse commonResponse = client.orderDetail(275110136488455424L);
+        System.out.println("result:"+commonResponse);
+    }
+    @Test
+    public void orderList() {
+        Map<String, String> params = new HashMap<>();
+        FutureCommonResponse commonResponse = client.orderList(params);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void orderCancel() {
+        FutureCommonResponse commonResponse = client.orderCancel(275110136488455424L);
+        System.out.println("result:"+commonResponse);
+    }
+
+    @Test
+    public void allCancel() {
+        FutureCommonResponse commonResponse = client.allCancel(null);
+        System.out.println("result:"+commonResponse);
+    }
+
+
+}
