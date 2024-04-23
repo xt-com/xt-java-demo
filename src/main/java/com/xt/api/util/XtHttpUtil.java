ec6673ebb5e26df8d4f39ac5a1ddc70f5f795598
lina.xie <943714607@qq.com>
2024-04-23 14:07:36 +0800

    modify base url

--- src/main/java/com/xt/api/util/XtHttpUtil.java
+++ src/main/java/com/xt/api/util/XtHttpUtil.java
@@ -17,7 +17,7 @@ public class XtHttpUtil {
     private static final String secretKey = "79bb9a1fece8a8dd6cb7c89aa85ccc2d8ca116f0";
     private static final String encry = "HmacSHA256";
     private static final String contentType = "application/json";
-    private static final String baseUrl = "http://oapi.xt-qa.com";
+    private static final String baseUrl = "https://oapi.xt.com";
     private static final String window = "6000";
     private static final String accessToken = "eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI1MzU2OTIwMTExOTg0IiwiYWNjb3VudC1pZCI6NTM1NjkyMDExMTk4NCwidXNlci1pZCI6NTM1NjkyMDExMTk4NCwic2NvcGUiOiJ1c2VyaW5mbyIsInNpZ24tdHlwZSI6IlVQIiwiYWNjb3VudC1sZXZlbCI6MSwiZXhwIjoxNzExNjA5MzUzLCJjbGllbnRfaWQiOiJEREo3QkxLNDlZVUNMOTdTIiwidGVuYW50LWlkIjoxfQ.QPkkyI8MOKya9zrEiZGOkE8Qaq1gNFMwd6Fro1Iob93Rm_MRk5nfo-NM_cQZ8CId4TfYM2OtnwhDe4OfkAsrY8j43aXruniKkc5Z_4hGx14EjkzX_SbLnXDHL-ztuBWuAT9nMfl84pBbKBGRdEmlBLPHiS5qi1FHb6EXdPJeYpI";
     static {
