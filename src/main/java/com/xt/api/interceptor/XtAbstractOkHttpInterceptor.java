a756599b7d691a2d34b4e2c4a6cf772279fe1984
jielina <943714607@qq.com>
2024-07-19 18:17:36 +0800

    add copytrade api

--- src/main/java/com/xt/api/interceptor/XtAbstractOkHttpInterceptor.java
+++ src/main/java/com/xt/api/interceptor/XtAbstractOkHttpInterceptor.java
@@ -19,12 +19,8 @@ import java.util.stream.Collectors;
  * @create 2023/9/21 10:43
  */
 public abstract class XtAbstractOkHttpInterceptor implements Interceptor {
-
-//    protected static final String appKey = "ef07dd0c-df05-450d-8ea9-ee4c4e2c2539";
-//    protected static final String secretKey = "4a4e753fca66ef4ec23726646c46e294237e7d78";
-
-    protected static final String appKey = "69dba614-6817-406a-88f7-40a289a72304";
-    protected static final String secretKey = "2bb5edbfb4f22246741acbe88235b77b9338184d";
+    protected static final String appKey = "xxx";
+    protected static final String secretKey = "xxx";
 
 
     protected static final String encry = "HmacSHA256";
